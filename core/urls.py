b8faa7c6e051bc8011500e12cf83d2cad795fb43
LeRodrigues2005 <leticiadepatta@gmail.com>
2025-02-17 11:53:51 -0300

    url redirection fixed

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 from django.urls import path
 from .api import api
 
+# Automatically redirects to /api/docs when accessing the site root
+def redirect_to_docs(request):
+    return HttpResponseRedirect('/api/docs')
+
 urlpatterns = [
+    path('', redirect_to_docs),  # Redirect root to /api/docs
     path('admin/', admin.site.urls),
     path('api/', api.urls)
 ]
